5ebea8428bcaf8664feea7fd05cfee5cb15df105
Bernd Porr <mail@berndporr.me.uk>
2020-01-30 09:19:13 +0000

    Changed to pointers in the demo program

--- ad7705_test.cpp
+++ ad7705_test.cpp
@@ -35,11 +35,13 @@ class AD7705printSampleCallback : public AD7705callback {
 // Registers the callback.
 // Prints data till the user presses a key.
 int main(int argc, char *argv[]) {
-	AD7705Comm ad7705comm;
-	AD7705printSampleCallback ad7705printSampleCallback;
-	ad7705comm.setCallback(&ad7705printSampleCallback);
-	ad7705comm.start(AD7705Comm::SAMPLING_RATE_50HZ);
+	AD7705Comm* ad7705comm = new AD7705Comm();
+	AD7705printSampleCallback* ad7705printSampleCallback = new AD7705printSampleCallback();
+	ad7705comm->setCallback(ad7705printSampleCallback);
+	ad7705comm->start(AD7705Comm::SAMPLING_RATE_50HZ);
 	getchar();
-	ad7705comm.stop();
+	ad7705comm->stop();
+	delete ad7705comm;
+	delete ad7705printSampleCallback;
 	return 0;
 }
