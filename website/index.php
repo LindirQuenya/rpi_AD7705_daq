e7f0818444c490c9c2a0fabc1bb1e14951415878
Bernd Porr <mail@berndporr.me.uk>
2020-03-19 13:14:39 +0000

    Now with dygraph

--- website/index.php
+++ website/index.php
@@ -1,50 +1,88 @@
 <!DOCTYPE html>
 <html>
-<head>
-<title>Welcome to the Realtime Embedded Test server</title>
-<style>
-    body {
-        width: 35em;
-        margin: 0 auto;
-        font-family: Tahoma, Verdana, Arial, sans-serif;
-    }
-</style>
-</head>
-<body>
-  <h1>Welcome to Realtime Embedded</h1>
+    <head>
+	<title>Welcome to the Realtime Embedded Test server</title>
+	<style>
+	 body {
+             width: 35em;
+             margin: 0 auto;
+             font-family: Tahoma, Verdana, Arial, sans-serif;
+	 }
+	</style>
+	<script src="//cdnjs.cloudflare.com/ajax/libs/dygraph/2.1.0/dygraph.min.js"></script>
+	<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/dygraph/2.1.0/dygraph.min.css" />
+    </head>
+    <body>
+	<h1>Welcome to Realtime Embedded</h1>
 
-  <?php
-   //Create a UDP socket
-   if(!($sock = socket_create(AF_INET, SOCK_DGRAM, 0)))
-   {
-        $errorcode = socket_last_error();
-        $errormsg = socket_strerror($errorcode);
-   
-        die("Couldn't create socket: [$errorcode] $errormsg \n");
-    }
+	<?php
+	// PHP code
+	//Create a UDP socket
+	if(!($sock = socket_create(AF_INET, SOCK_DGRAM, 0)))
+	{
+            $errorcode = socket_last_error();
+            $errormsg = socket_strerror($errorcode);
+	    
+            die("Couldn't create socket: [$errorcode] $errormsg \n");
+	}
 
-    // Bind the source address
-    if( !socket_bind($sock, "127.0.0.1" , 65000) )
-    {
-        $errorcode = socket_last_error();
-        $errormsg = socket_strerror($errorcode);
+	// Bind the source address
+	if( !socket_bind($sock, "127.0.0.1" , 65000) )
+	{
+            $errorcode = socket_last_error();
+            $errormsg = socket_strerror($errorcode);
 
-        die("Could not bind socket : [$errorcode] $errormsg \n");
-    }
+            die("Could not bind socket : [$errorcode] $errormsg \n");
+	}
 
-    echo "<h2>LM35 emperature</h2>";
-    for ($x = 0; $x <= 10; $x++) {
-        //Receive some data
-        $r = socket_recvfrom($sock, $buf, 512, 0, $remote_ip, $remote_port);
-        $temperature = (float)$buf / 65536 * 2.5 * 0.6 * 100;
-	echo "<p>".$temperature."C</p>";
-    }
+	$temperature = 0;
+	$navg = 10;
+	echo "<h2>LM35 temperature sensor connected to the PI</h2>";
+	for ($x = 0; $x <= $navg; $x++) {
+            //Receive some data
+            $r = socket_recvfrom($sock, $buf, 512, 0, $remote_ip, $remote_port);
+            $temperature = $temperature + (float)$buf / 65536 * 2.5 * 0.6 * 100;
+	}
+	$temperature = $temperature / $navg;
+	socket_close($sock);
 
-    // this data could now be fed into a java script plotting program		   
+	echo "<p>Current temperature in Bernd's house: ".$temperature." &#8451;</p>";
 
-    socket_close($sock);
+	$output = fopen("data.txt",'a') or die("Can't save");
+	$p = array(time(),$temperature);
+	fputcsv($output, $p);
+	fclose($output) or die("Can't close");
 
-?>
-  
-</body>
+	?>
+
+	<div id="graphdiv"></div>
+	<script type="text/javascript">
+	 g = new Dygraph(
+	     document.getElementById("graphdiv"),
+	     "data.txt",
+             {
+                 legend:'always',
+                 ticker: Dygraph.dateTicker
+             }
+	 );
+	</script>
+
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+
+<p><a href="http://dygraphs.com/">dygraphs</p>
+
+<br />
+
+<p><a href="textonly.php">Text only version</a></p>
+	
+    </body>
 </html>
